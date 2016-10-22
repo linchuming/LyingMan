4fe8d38c196ce76fcd6455d804397e0fe90fc0d9
IsaWdx <isachriswang@163.com>
2016-10-22 18:13:32 +0800

    Delete unused injection

--- web/app.js
+++ web/app.js
@@ -1,3 +1,3 @@
 (function() {
-	angular.module("lyingman", ["ui.router","ngRoute"]);
+	angular.module("lyingman", ["ngRoute"]);
 })()
--- web/app.route.js
+++ web/app.route.js
@@ -1,12 +1,5 @@
 (function() {
-	angular.module("lyingman").config(function($stateProvider, $urlRouterProvider,$routeProvider){
-    // $stateProvider
-    //     .state('login', {
-		//
-    //         templateUrl: 'app/component/login.html',
-		// 				controller: 'loginController'
-    //     })
-    //     $urlRouterProvider.otherwise('login');
+	angular.module("lyingman").config(function($routeProvider){
 		$routeProvider
     .when("/login", {
 			templateUrl: 'app/component/login.html',
