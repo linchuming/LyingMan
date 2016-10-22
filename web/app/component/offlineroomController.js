17d0b5f7c52e89d192ffbbe73e764487b6d2e7e8
IsaWdx <isachriswang@163.com>
2016-10-22 22:14:49 +0800

    Add god message

--- web/app/component/offlineroomController.js
+++ web/app/component/offlineroomController.js
@@ -5,6 +5,47 @@
     $scope.currentConversation = '';
     $scope.onlinenumber = 1;
     $scope.myMessage = '';
+    $scope.isGod = false;
+    var initializeRoom = function() {
+      var obj = {
+        type: 'join_offroom',
+        data: {
+          number: userInfoModel.roomnumber,
+          id: userInfoModel.id,
+          token: userInfoModel.token
+        }
+      };
+      webSocketService.send(JSON.stringify(obj));
+      webSocketService.onmessage(function(msg) {
+        var obj = JSON.parse(msg.data);
+        if(obj.type != undefined) {
+          var data = obj.data;
+          switch (obj.type) {
+            case 'text_message':
+              //console.log(data);
+              messageHtml(data.id, data.name, data.message);
+              break;
+            case 'offroom_number':
+              $scope.onlinenumber = data.number;
+              $scope.$apply();
+              break;
+            case 'role':
+              if(data.role_id == '0') {
+                $scope.isGod = true;
+                //messageHtml(-1, "系统消息", "您是本间房主/上帝，当人数已满,请点击 <div ng-click='startGame()' class='link'>开始游戏</div>")
+                $scope.$apply();
+              }
+              else {
+
+              }
+            default:
+
+          }
+        }
+      });
+    }
+
+
     var messageHtml = function (id, name, phrase) {
       var prefix = id == userInfoModel.id ? "<dl class='me m1'><dt>" : "<dl class='he m1'><dt>";
       var phrase = prefix + name + "</dt><dd>" + phrase + "</dd></dl>";
@@ -13,33 +54,21 @@
       $scope.trustedCurrentConversation = $sce.trustAsHtml($scope.currentConversation);
       $scope.$apply();
     }
-    var obj = {
-      type: 'join_offroom',
-      data: {
-        number: userInfoModel.roomnumber,
-        id: userInfoModel.id,
-        token: userInfoModel.token
-      }
-    };
-    webSocketService.send(JSON.stringify(obj));
-    webSocketService.onmessage(function(msg) {
-      var obj = JSON.parse(msg.data);
-      if(obj.type != undefined) {
-        var data = obj.data;
-        switch (obj.type) {
-          case 'text_message':
-            //console.log(data);
-            messageHtml(data.id, data.name, data.message);
-            break;
-          case 'offroom_number':
-            $scope.onlinenumber = data.number;
-            $scope.$apply();
-            break;
-          default:
 
+    var startGame = function() {
+      var obj = {
+        type: 'offroom_start',
+        data: {
+          id: userInfoModel.id,
+          token: userInfoModel.token
         }
-      }
-    });
+      };
+      console.log("user");
+      webSocketService.send(JSON.stringify(obj));
+    }
+
+
+
     var sendMessage = function() {
       var obj = {
         type: 'offroom_message',
@@ -52,7 +81,9 @@
       webSocketService.send(JSON.stringify(obj));
       $scope.myMessage = '';
     };
+    $scope.startGame = startGame;
     $scope.sendMessage = sendMessage;
+    initializeRoom();
 
 	}
 	angular.module('lyingman').controller('offlineroomController', offlineroomController);
